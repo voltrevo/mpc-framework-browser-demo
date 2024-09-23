1f884efd45ef9d7e872e940567c5e1600f7c4e28
Andrew Morris <voltrevo@gmail.com>
2024-09-23 16:59:30 +1000

    npm create vite@latest

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>See console logs (cmd+option+j)</div>
+      <div>(Or maybe ctrl+shift+j)</div>
+    </>
+  )
+}
+
+export default App
